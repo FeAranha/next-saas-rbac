8cfd4ab667a778b966fa786e2c0924b684ad08c9
FeAranha <felipearanha.c@gmail.com>
2024-06-19 13:13:04 -0300

    Listando organizações

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -2,7 +2,7 @@ import { Header } from '@/components/header'
 
 export default async function Home() {
   return (
-    <div className="py-4">
+    <div className="p-4">
       <Header />
       <main></main>
     </div>
