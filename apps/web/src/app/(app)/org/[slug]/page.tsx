8892df163c9a9db4b53c3ace96bce7178227e69e
FeAranha <felipearanha.c@gmail.com>
2024-06-21 14:38:12 -0300

    Interception routes no Next.js

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -4,7 +4,7 @@ export default async function Projects() {
   return (
     <div className="space-y-4 py-4">
       <Header />
-      <h1>Projects</h1>
+      <main className="mx-auto w-full max-w-[1200px]">Projects</main>
     </div>
   )
 }
