71eb1e6e04f63a44c77d7ddd17ddda03609cf201
FeAranha <felipearanha.c@gmail.com>
2024-07-23 13:13:49 -0300

    Navegação do app por abas

--- apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
@@ -1,10 +1,7 @@
-import { Header } from '@/components/header'
-
 export default async function Project() {
   return (
     <div className="space-y-4 p-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">Project</main>
+      <h1 className="text-2xl font-bold">Project</h1>
     </div>
   )
 }
